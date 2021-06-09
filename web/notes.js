5e666f9821d4031e0220475dbd875757d3fd7cac
Mohit Anand <anandmohit7@gmail.com>
2021-06-09 23:09:06 +0530

    add notes

---
+++ web/notes.js
@@ -0,0 +1,23 @@
+//*************************************************************************************************** */
+//Worker to Main Page
+//In Main page
+worker.addEventListener("message", (evt)=> {
+    console.log(evt.data)
+})
+//And in workers file
+self.postMessage("Hello")
+//*************************************************************************************************** */
+//Min page to worker
+//In Main Page
+worker.addEventListener("message", (evt)=> {
+    console.log(evt.data)
+    worker.postMessage('hello from the client')
+})
+//In worker
+self.onmessage = onMessage;
+function onMessage(evt) {
+	console.log(`Received in web worker ${evt.data}`)
+}
+//*************************************************************************************************** */
+//To Terminate
+worker.terminate()
